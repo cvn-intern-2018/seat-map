67b8e02af5143b460bab95ab60b3a246a18e9633
Nguyen Anh Nam <n001293@cb.local>
2018-07-17 13:04:53 +0700

    Initialize project

---
+++ resources/views/admin/navigation.blade.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul class="admin-menu">
+        <li class="menu-item"><a href="#">Seat map management</a></li>
+        <li class="menu-item"><a href="#">User management</a></li>
+        <li class="menu-item"><a href="#">Group management</a></li>
+        <li class="menu-item"><a href="{{ route('logout') }}">Log out</a></li>
+    </ul>
+</nav>
